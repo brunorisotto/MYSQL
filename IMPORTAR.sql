0409840193ca568deb058441e360a8b7868419bf
brunorisotto <86900256+brunorisotto@users.noreply.github.com>
2022-02-09 15:08:32 -0300

    Delete IMPORTAR.sql

--- IMPORTAR.sql
+++
@@ -1,3 +0,0 @@
-select * from sakila.actor;  #seleccionar todo ( importar )
-select actor_id,first_name,last_name,last_update from sakila.actor; #importar lo necesario
-select actor_id, first_name as Nombre, last_name as Apellido, last_name from sakila.actor; # cambiar nommbre a tablas
