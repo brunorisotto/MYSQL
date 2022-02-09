708a9613314d2956a8ddc7f2b0d3f5dbe469c206
brunorisotto <86900256+brunorisotto@users.noreply.github.com>
2022-02-09 15:08:50 -0300

    Delete consultarValores_OrdenarTablas.sql

--- consultarValores_OrdenarTablas.sql
+++
@@ -1,3 +0,0 @@
-select distinct(store_id) from sakila.customer; # distinct - consultar valores unicos de una tabla
-select distinct(customer_id)from sakila.payment;
-select * from sakila.customer order by first_name ASC; # ordenar tablas (order by) (de forma ASC-Ascendente ; DESC - Descentende)
